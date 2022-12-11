22ce956c226b18b9456084cf1a5c7c39d1e005d0
aaronhdcomps <aaron.hdcomps@gmail.com>
2022-12-11 13:46:11 -0800

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,14 @@
 // containing all the numbers from start up to (and including) end.
 // Example output:
 // console.log(range(1, 10)); // outputs [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
+function range(start, end) {
+    let array = [];
+    for (let i = start; i <= end; i++) {
+        array.push(i);
+    }
+    return array;
+}
+console.log(range(5, 25));
 // 2. Next, write a sum function that takes an array of numbers and returns the sum of
 // these numbers. Run the example program and see whether it does indeed return 55.
 // Example output:
