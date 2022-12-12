9dd3f81f3d1ca761863c440b63046204f02aa71f
aaronhdcomps <aaron.hdcomps@gmail.com>
2022-12-11 17:38:54 -0800

    Update script.js

--- script.js
+++ script.js
@@ -15,6 +15,7 @@ console.log(range(2, 25));
 // these numbers. Run the example program and see whether it does indeed return 55.
 // Example output:
 // console.log(sum(range(1, 10))); // returns 55
+
 function sum(numArray) {
     let total = 0;
     for (let i = 0; i < numArray.length; i++) {
@@ -36,6 +37,25 @@ console.log(sum(range(2, 25)));
 // let arrayValue = [1, 2, 3, 4, 5];
 // reverseArrayInPlace(arrayValue);
 // console.log(arrayValue); // outputs [5, 4, 3, 2, 1]
-function reverseArray(numArray) {
-    
+
+function reverseArray(inArray) {
+    let outArray = [];
+    for(let i = 0; i < inArray.length; i++){
+        outArray.unshift(inArray[i]);
+    }
+    return outArray;
 }
+let sample1 = ['A', 'B', 'C']
+let sample2 = [1, 2, 3, 4, 5]
+
+console.log(reverseArray(sample1));
+
+function reverseArrayInPlace(inArray) {
+    for(let i = 0; i <= inArray.length / 2; i++){
+        let val = inArray[i];
+        inArray[i] = inArray[inArray.length - 1 - i];
+        inArray[inArray.length - 1 - i] = val;
+    }
+    return inArray;
+}
+console.log(reverseArrayInPlace(sample2));
